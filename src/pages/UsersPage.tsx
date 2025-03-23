9677e4827d14847d56b91ae1586915041f947411
underrdog1 <theblessedunderdog@gmail.com>
2025-03-23 18:42:10 +0100

    styling changed

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -4,7 +4,12 @@ import { Link } from "react-router-dom";
 import './UsersPage.css';
 
 const UsersPage: React.FC = () => {
-  return (<><Link to={"/add-user"} className="add-user-link">Add User</Link><UserList /></>);
+  return (<div
+  style={{ display: "flex", flexDirection: "column", alignItems: "center" }}><Link to={"/add-user"} className="add-user-link">
+    Add User
+  </Link>
+  <UserList />
+  </div>);
 };
 
 export default UsersPage;
